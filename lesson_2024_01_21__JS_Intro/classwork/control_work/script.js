f9c519fa77663a720c56e7727ff81bcb2923b883
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2024-01-25 22:25:42 +0300

    update

---
+++ lesson_2024_01_21__JS_Intro/classwork/control_work/script.js
@@ -0,0 +1,29 @@
+var simbol = '*';
+var space = ' ';
+var line = '';
+var count = 8;
+
+for (let index = 0; index < count; index++) {
+    line += space;
+    console.log(line + simbol);
+}
+
+console.log("---------------------")
+
+var line = '';
+
+for (let index = 0; index < count; index++) {
+    line += simbol;
+    console.log(line);
+}
+
+console.log("---------------------")
+
+var line = '';
+
+for (let index_space = count - 1; index_space <= 0; index_space--) {
+    line_space += 
+    line += simbol;
+    console.log(line);
+}
+
---
+++ lesson_2024_01_21__JS_Intro/classwork/work/script.js
@@ -0,0 +1,116 @@
+// console.log('Helo world!')
+
+// var x = 5;
+// console.log(x)
+// var x = true;
+// console.log(x)
+// var x = "text";
+// console.log(x)
+
+// var x;
+// // x = 5;
+// // console.log(x);
+// console.log(typeof(x));
+
+// var x;
+// x = "word";
+// console.log(x);
+// console.log(typeof(x));
+
+// var x;
+// x = "w";
+// console.log(x);
+// console.log(typeof(x));
+
+// var x = [1, true, 'word', 4.5, function test() {console.log("bla-bla")}, [1, 2, 3]];
+// console.log(x);
+// console.log(typeof(x));
+
+// var x = {name: 'John', age: 31};
+// console.log(x);
+// console.log(typeof(x));
+
+// var x = 10 / 0;
+// console.log(x);
+// console.log(typeof(x));
+
+// var x = 'word' / 5;
+// console.log(x);
+// console.log(typeof(x));
+
+// var x = 10;
+// if (true) {
+//     let x = 5;
+//     console.log(x);
+// }
+// console.log(x);
+// console.log(window.x);
+
+// let x = '2';
+// if (x == 2) {
+//     console.log('True');
+// } else console.log('False');
+
+// for (let index = 0; index < 5; index++) {
+//     console.log(index);
+// }
+
+// while (false) {
+//     console.log('1');
+// }
+
+// do {
+//     console.log('2');
+// } while (false);
+
+// function print() {
+//     console.log('word');
+// }
+
+// print();
+
+// function print(n, s) {
+//     return n + ' ' + s;
+// }
+
+// let x = print(1);  // , 'two', [3, 4, 5]
+// console.log(x);
+
+// function print() {
+//     for (let index = 0; index < arguments.length; index++) {
+//         console.log(index + ') ' + arguments[index]);
+//     }
+//     return arguments[0];
+// }
+
+// let x = print(1 , 'two', [3, 4, 5]);
+// console.log(x);
+
+// alert ("Hello!")
+// prompt ("How are you?")
+// console.log(confirm("Are you a boy?"));
+
+// var my_number = parseInt("123abc")
+// console.log(my_number + 2);
+
+// var x = 3 + '5';
+// console.log(x);
+// console.log(typeof(x));
+
+var x = '5' - 3;
+console.log(x);
+console.log(typeof(x));
+var x = 3 + '5' - 3;
+console.log(x);
+console.log(typeof(x));
+var x = '1' + 6 / 2;
+console.log(x);
+console.log(typeof(x));
+var x = ('1' + 6) / 2;
+console.log(x);
+console.log(typeof(x));
+
+var x = + '5' + 3;
+console.log(x);
+console.log(typeof(x));
+
