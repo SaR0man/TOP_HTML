1c1df193b2209ec58198190e26c522ac80091b92
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2024-02-20 22:37:20 +0300

    update

--- lesson_2024_02-17 - JS--Ajax__Fetch API/script.js
+++ lesson_2024_02-17 - JS--Ajax__Fetch API/script.js
@@ -19,17 +19,17 @@ async function searchByTitle(title, page = 1) {
 
 let myForm = document.forms.movieSearch;
 let list = document.querySelector(".list");
+let title;
+let page = 1;
 
-myForm.addEventListener("submit", async () => {
-    event.preventDefault();
-    let title = myForm.title.value;
-    let data = await searchByTitle(title);
+async function renderMovieByTitle(title, page) {
+    let data = await searchByTitle(title, page);
 
     list.innerHTML = '';
     for (const item of data.Search) {
         list.innerHTML += `
         <div class="card col-4">
-            <img src="${item.Poster}" class="card-img-top" alt="...">
+            <img src="${item.Poster}" onerror="event.target.src='./img/image-not-found.jpg'"  class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${item.Title}</h5>
                 <p class="card-text">Type: ${item.Type}<br>Year: ${item.Year}</p>
@@ -38,7 +38,38 @@ myForm.addEventListener("submit", async () => {
         `;
     }
 
-    console.log(data);
+    let count = data.totalResults;
+    let sizePage = Math.ceil(count / 10);
 
+    renderPagination(sizePage);
+
+    console.log(data);
+}
+
+myForm.addEventListener("submit", async () => {
+    event.preventDefault();
+
+    title = myForm.title.value;
+
+    await renderMovieByTitle(title, page);
+    
     myForm.reset();
+
 });
+
+let ul = document.querySelector('.pagination')
+
+function renderPagination(page) {
+    ul.innerHTML = '';
+    ul.innerHTML += '<li class="page-item"><a class="page-link">Previous</a></li>';
+    for (let i = 1; i <= page; i++) {
+        ul.innerHTML += `<li class="page-item"><a class="page-link">${i}</a></li>`;
+    }
+    ul.innerHTML += '<li class="page-item"><a class="page-link">Next</a></li>';
+}
+
+document.querySelector('.pagination').addEventListener('click', async ()=>{
+    await renderMovieByTitle(title, event.target.innerText);
+})
+
+// document.querySelectorAll('img').addEventListener('error')
