4b22d129a202e530fec3dbb0641c5cac0c41fcda
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2024-03-10 22:02:17 +0300

    update

--- lesson_2024_03-03/classwork_v5/demo/src/main/java/com/example/demo/conrollers/TestController.java
+++ lesson_2024_03-03/classwork_v5/demo/src/main/java/com/example/demo/conrollers/TestController.java
@@ -1,17 +1,16 @@
-package com.example.demo.controllers;
+package com.example.demo.conrollers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("test")
 
 public class TestController {
-
     @GetMapping("one")
-
-    public String one () {
-        return ("Hello world!");
+    public String One() {
+        return ("Hello Word!");
     }
 }
