ff86ff2ae5c8c96de8fc7b6d41c779be406622a7
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2024-03-22 23:35:31 +0300

    update

--- lesson_2024_03_17 - CRUD/classwork/demo/src/main/java/com/example/demo/controllers/TestController.java
+++ lesson_2024_03_17 - CRUD/classwork/demo/src/main/java/com/example/demo/controllers/TestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.models.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -25,7 +26,7 @@ public class TestController {
     //// метод создает коллекцию work и добавляет объект Person
     private void initList() {
         work = new ArrayList<>();
-        work.add(new Person("John", 1));
+//        work.add(new Person("John", 1));
     }
 
     ////// Запросы:
@@ -35,9 +36,14 @@ public class TestController {
     }
 
     @GetMapping("/add")
-    public boolean addUser(String name, int id) {
-        work.add(new Person(name, id));
-        return true;
+    public boolean addUser(@RequestParam(value = "name", required = false) String field_name, int id) {
+        if (field_name == null) return false;
+        Person person = (Person) work.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        if (person == null) {
+            work.add(new Person(field_name, id));
+            return true;
+        }
+        else return false;
     }
 
     @GetMapping("/clear")
