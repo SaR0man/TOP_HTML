b3f5df165011e0d31af31d5d157912d7ff118f9f
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2024-03-24 21:25:17 +0300

    update

--- lesson_2024_03_23 - Jar/classwork/Second/src/Main.java
+++ lesson_2024_03_23 - Jar/classwork/Second/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person();
+        person.name = "John";
+        person.surname = "Kennedy";
+        person.age = 52;
+
+        System.out.println(person);
     }
 }
+
