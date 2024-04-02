bc899cc01539c499aa42780fe4eecc346ac17d2c
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2024-04-02 22:23:34 +0300

    update

--- lesson_2024_03_24 - Annotation/homework/src/Main.java
+++ lesson_2024_03_24 - Annotation/homework/src/Main.java
@@ -1,5 +1,45 @@
+import java.lang.annotation.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface Audit {
+}
+
+class Cat {
+    @Audit
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+
+    public void eat(String food) {
+        System.out.println("Eating " + food);
+    }
+
+    @Audit
+    public void sleep(int duration, String place) {
+        System.out.println("Sleeping for " + duration + " hours in " + place);
+    }
+}
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        Cat cat = new Cat();
+        Class catClass = cat.getClass();  // Получение объекта класса Class для объекта cat (для работы рефлексии)
+        Method[] methods = catClass.getMethods();  // Получение массива методов, имеющихся в классе Cat (через рефлексию)
+        for (Method method : methods) {  // перебираем все методы класса
+            Annotation[] annotations = method.getAnnotations();
+            for (Annotation annotation : annotations) {
+                if (annotation instanceof Audit) {
+                    System.out.println("*** Method with @Audit annotation found: \"" + method.getName() + "\"");
+                    System.out.print("*** Method arguments: ");
+                    for (Class<?> parameterType : method.getParameterTypes()) {
+                        System.out.print("\"" + parameterType.getName() + "\", ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
     }
 }
