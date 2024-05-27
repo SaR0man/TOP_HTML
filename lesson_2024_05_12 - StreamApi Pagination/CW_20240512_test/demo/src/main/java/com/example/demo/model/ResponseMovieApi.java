af82b3597b50a1a7c18ce1be59706e6dd4e37887
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2024-05-27 22:22:11 +0300

    update

--- lesson_2024_05_12 - StreamApi Pagination/CW_20240512_test/demo/src/main/java/com/example/demo/model/ResponseMovieApi.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model;
-
-import java.util.ArrayList;
-
-public class ResponseMovieApi {
-    public String totalResults;
-    public String Response;
-    public ArrayList<Movie> Search;
-}
